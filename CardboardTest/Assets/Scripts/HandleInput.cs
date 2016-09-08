d3e966ad00ab2ba8abbfe7cf2eec7bde9da5543f
BLACKROCK92 <perezdaffunchio@gmail.com>
2016-09-08 16:41:02 -0300

    1st Commit

---
+++ CardboardTest/Assets/Scripts/HandleInput.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class HandleInput : MonoBehaviour, IGvrGazeResponder
+{
+    public float force;
+
+    void Start()
+    {
+        GetComponent<Rigidbody>().AddForce(0, force, 0);
+    }
+
+    void Update()
+    {
+
+    }
+
+    void Triggered()
+    {
+        Debug.Log("Gilada");
+        Destroy(this, 1f);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "FloorCollider")
+        {
+            Destroy(this);
+        }
+    }
+
+    #region IGvrGazeResponder Implementation
+    public void SetGazedAt(bool gazedAt)
+    {
+        GetComponent<Renderer>().material.color = gazedAt ? Color.green : Color.red;
+    }
+
+    public void OnGazeEnter()
+    {
+        SetGazedAt(true);
+    }
+
+    public void OnGazeExit()
+    {
+        SetGazedAt(false);
+    }
+
+    public void OnGazeTrigger()
+    {
+        Triggered();
+    }
+    #endregion
+}
---
+++ CardboardTest/Assets/Scripts/SpawnObjects.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnObjects : MonoBehaviour
+{
+    public float startWait;
+    public float waveWait;
+    public float spawnWait;
+    public int objectCount;
+    public GameObject[] spawners = new GameObject[8];
+    public GameObject objectPrefab;
+
+    void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    IEnumerator Spawn()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < objectCount; i++)
+            {
+                Vector3 spawnPosition = spawners[Random.Range(0, 7)].transform.position;
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(objectPrefab, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+        }
+    }
+
+    void Update()
+    {
+
+    }
+}
